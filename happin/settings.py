4ec05966ff3041913f0c70b17549f59d7ef230bf
JP Harris <jpharris.contact@gmail.com>
2025-10-14 23:45:36 -0400

    adds security mesaures

--- happin/settings.py
+++ happin/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-ziu+z53k(n+9fusyt*ry&gy#fqn7j7-ps=-)$rmw#jq^pb!mo@')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
