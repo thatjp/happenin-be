f948056e03ff1ab13c4a87c8789a697594c489eb
JP Harris <jpharris.contact@gmail.com>
2025-10-15 15:12:04 -0400

    Create nginx conf

--- happenin/settings.py
+++ happenin/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0,*').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
