3dbfa6e42b4250265f40d2a678e0486487d128f1
JP Harris <jpharris.contact@gmail.com>
2025-10-16 00:01:23 -0400

    Updates static files locatiosn

--- happenin/settings.py
+++ happenin/settings.py
@@ -141,7 +141,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
