34d88fe9709b0b81e3a9862f523204d63c51a06d
JP Harris <jpharris.contact@gmail.com>
2025-10-15 22:41:53 -0400

    rename static files dir

--- happenin/settings.py
+++ happenin/settings.py
@@ -141,7 +141,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
